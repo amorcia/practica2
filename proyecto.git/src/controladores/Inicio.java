737c0e7fe209a164c1106d64bb0c9b3331c8c3ce
amorcia <antoniowebserver@gmail.com>
2025-01-15 09:57:42 +0100

    Primera subida a la rama master

---
+++ proyecto.git/src/controladores/Inicio.java
@@ -0,0 +1,63 @@
+package controladores;
+
+import java.util.Scanner;
+
+public class Inicio {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		boolean esCerrado = false;
+		byte opcionUsuario;
+		int numero1 = 0;
+		int numero2 = 0;
+		int total;
+		do {
+		System.out.println("-------MENÚ-------");
+		System.out.println("0.Cerrar Menú");
+		System.out.println("1.Sumar");
+		System.out.println("2.Restar");
+		System.out.println("3.Multiplicar");
+		System.out.println("----------------");
+		System.out.println("Elija la opción deseada: ");
+		opcionUsuario = scanner.nextByte();
+		
+		switch (opcionUsuario) {
+		case 0: {
+			System.out.println("Cerrando menú...");
+			esCerrado = true;
+		}
+		case 1: {
+			System.out.println("Introduzca el primer número: ");
+			numero1 = scanner.nextInt();
+			System.out.println("Introduzca el segundo número: ");
+			numero2 = scanner.nextInt();
+			System.out.println("Sumando...");
+			total = numero1 + numero2;
+			System.out.println("La suma en total es: " + total);
+		}
+		case 2: {
+			System.out.println("Introduzca el primer número: ");
+			numero1 = scanner.nextInt();
+			System.out.println("Introduzca el segundo número: ");
+			numero2 = scanner.nextInt();
+			System.out.println("Restando...");
+			total = numero1 - numero2;
+			System.out.println("La suma en total es: " + total);
+		}
+		case 3: {
+			System.out.println("Introduzca el primer número: ");
+			numero1 = scanner.nextInt();
+			System.out.println("Introduzca el segundo número: ");
+			numero2 = scanner.nextInt();
+			System.out.println("Multiplicando...");
+			total = numero1 * numero2;
+			System.out.println("La suma en total es: " + total);
+		}
+		default:
+			System.out.println("Opción inexistente");
+		}
+
+		}while(!esCerrado);
+	}
+
+}
